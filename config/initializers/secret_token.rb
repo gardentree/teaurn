e32824d1949e04de02d829612cb6b6a7462a4c6c
GardenTree <takahiro.ueki@gmail.com>
2013-01-28 23:47:26 +0900

    Execute [rails new teaurn -T]

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teaurn::Application.config.secret_token = '3ff17cb5ec4772a1394e6199befdd16577cfb2f8e857c4132e88c0e4016dda4103d8efa3c56687402b9a7adb672cf6810bd56beb6f288af3d839ccf282c504d3'
